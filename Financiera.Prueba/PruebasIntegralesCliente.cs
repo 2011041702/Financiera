fe731e5fd564d418ae9b6a41162d49dd4d862b2d
2011041702 <chris_yeah90@hotmail.com>
2016-04-13 21:37:19 -0500

    Financiera

---
+++ Financiera.Prueba/PruebasIntegralesCliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Financiera.Dominio;
+using Financiera.Infraestructura;
+
+namespace Financiera.Prueba
+{
+    [TestClass]
+    public class PruebasIntegralesCliente
+    {
+        [TestMethod]
+        public void AgregarCliente()
+        {
+            var lo_cliente = Cliente.Agregar(1, "Christian Poma", DateTime.Parse("22/08/1992"));
+            using (var lo_contexto = new FinancieraContexto())
+            {                
+                lo_contexto.Clientes.Add(lo_cliente);
+                lo_contexto.SaveChanges();                
+            }
+            Assert.IsTrue(lo_cliente.CodigoCliente == 1);
+        }
+    }
+}
---
+++ Financiera.Prueba/PruebasUnitariasCliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Financiera.Dominio;
+
+namespace Financiera.Prueba
+{
+    [TestClass]
+    public class PruebasUnitariasCliente
+    {
+        [TestMethod]
+        public void ClienteCreadoSatisfactoriamente()
+        {
+            var lo_cliente = Cliente.Agregar(1, "Christian Roger", DateTime.Parse("17-12-1990"));
+            Assert.IsNotNull(lo_cliente);
+            Assert.IsTrue(lo_cliente.CodigoCliente == 1);
+        }
+    }
+}
